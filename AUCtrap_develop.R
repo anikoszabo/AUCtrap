1e68805a1b0e3d77711d3e8afb53d585115cccfa
Szabo <aszabo@mcw.edu>
2021-11-11 10:09:34 -0600

    Update DESCRIPTION

--- AUCtrap_develop.R
+++ AUCtrap_develop.R
@@ -1,4 +1,9 @@
 library(devtools)
+library(desc)
 create_package("../AUCtrap")
 
 use_git()
+use_github()
+desc_add_author(given="Aniko", family="Szabo", email = "aszabo@mcw.edu",
+                role=c("aut","cre"))
+use_gpl3_license()
