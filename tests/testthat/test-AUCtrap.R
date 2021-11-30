dba017f251e4eb745a185892cc837c5f88306ece
Haoran Teng <hteng@mcw.edu>
2021-11-29 19:38:43 -0600

    iAUC related update

--- tests/testthat/test-AUCtrap.R
+++ tests/testthat/test-AUCtrap.R
@@ -1,6 +1,7 @@
 test_that("AUCtrap inputs are checked", {
   expect_error(AUCtrap(0:3, c(3,4,1,2), method="xyz"))
   expect_error(AUCtrap(0:4, c(3,4,1,2), method="AUC"))
+  expect_error(AUCtrap(0:4, c(3,4,1,2), method="iAUC"))
 })
 
 test_that("Total AUC calculation works", {
@@ -10,6 +11,16 @@ test_that("Total AUC calculation works", {
   expect_equal(AUCtrap(0:3, c(3,4,1,2)), ex)
   ex2 <- AUCtrap(rev(0:3), rev(c(3,4,1,2)), method="AUC")
   expect_equal(ex, ex2)
+
+})
+test_that("iAUC calculation works", {
+  ex <- AUCtrap(0:3, c(3,4,2,2), method="iAUC")
+  expect_equal(ex$value, 0.75)
+  expect_equal(ex$method, "iAUC")
+
+  ex2 <- AUCtrap(rev(0:3), rev(c(3,4,2,2)), method="iAUC")
+  expect_equal(ex, ex2)
+
 })
 
 test_that("Formula method works",{
@@ -19,9 +30,13 @@ test_that("Formula method works",{
   expect_equal(ex2, ex0)
   expect_error(AUCtrap(Value ~ Time + X, data=dd))
   expect_error(AUCtrap(Value ~ as.character(Time), data=dd))
+
+
 })
 
 test_that("Methods for 'auctrap' class work",{
   ex <- AUCtrap(0:3, c(3,4,1,2), method="AUC")
   expect_output(print(ex, digits=3), "7.5 using method AUC")
+  ex2 <- AUCtrap(0:3, c(3,4,2,2), method="iAUC")
+  expect_output(print(ex2, digits=3), "0.75 using method iAUC")
 })
